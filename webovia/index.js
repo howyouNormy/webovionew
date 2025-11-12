b93c9b7d0d905e30985f4683ada129cfbcd30dbc
howyouNormy <normytop@gmail.com>
2025-11-12 11:08:20 +0500

    Delete webovia directory

--- webovia/index.js
+++
@@ -1,9 +0,0 @@
-let btn=document.querySelector(".main_button");
-
-function Click_btn(){
-    let bd=document.querySelector("body");
-    bd.classList.toggle("body_style");
-    btn.classList.toggle("new_style");
-}
-
-btn.addEventListener("click",Click_btn);
